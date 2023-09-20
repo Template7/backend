9a2bc2dd457e1795fadc56f8e4946ddede845e23
allenkao <kaodondon7@gmail.com>
2023-09-20 18:11:34 +0800

    update

--- internal/route/handler/wallet.go
+++ internal/route/handler/wallet.go
@@ -4,7 +4,10 @@ import (
 	"github.com/Template7/backend/internal/t7Error"
 	"github.com/Template7/backend/internal/wallet"
 	"github.com/Template7/common/logger"
+	v1 "github.com/Template7/protobuf/gen/proto/template7/wallet"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/protobuf/encoding/protojson"
+	"io"
 	"net/http"
 )
 
@@ -33,14 +36,68 @@ func Deposit(c *gin.Context) {
 	log := logger.New().WithContext(c)
 	log.Debug("handle deposit")
 
-	// TODO: implementation
-	return
+	defer c.Request.Body.Close()
+	bd, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		log.WithError(err).Error("fail to read resp body")
+		c.JSON(http.StatusBadRequest, t7Error.InvalidBody.WithDetail(err.Error()))
+		return
+	}
+
+	unmarshaler := protojson.UnmarshalOptions{DiscardUnknown: true}
+	var req v1.DepositRequest
+	if err := unmarshaler.Unmarshal(bd, &req); err != nil {
+		log.WithError(err).With("resp", string(bd)).Error("fail to decode resp data")
+		c.JSON(http.StatusBadRequest, t7Error.DecodeFail.WithDetail(err.Error()))
+		return
+	}
+
+	if err := wallet.New().Deposit(c, c.Param("walletId"), req.GetCurrency(), req.GetAmount()); err != nil {
+		defer c.Abort()
+		log.WithError(err).Error("fail to update user info")
+		t7Err, ok := t7Error.ToT7Error(err)
+		if !ok {
+			log.WithError(err).Error("unknown error")
+			c.JSON(http.StatusForbidden, t7Error.InvalidToken)
+			return
+		}
+		c.JSON(t7Err.GetStatus(), t7Err)
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
 }
 
 func Withdraw(c *gin.Context) {
 	log := logger.New().WithContext(c)
 	log.Debug("handle withdraw")
 
-	// TODO: implementation
-	return
+	defer c.Request.Body.Close()
+	bd, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		log.WithError(err).Error("fail to read resp body")
+		c.JSON(http.StatusBadRequest, t7Error.InvalidBody.WithDetail(err.Error()))
+		return
+	}
+
+	unmarshaler := protojson.UnmarshalOptions{DiscardUnknown: true}
+	var req v1.WithdrawRequest
+	if err := unmarshaler.Unmarshal(bd, &req); err != nil {
+		log.WithError(err).With("resp", string(bd)).Error("fail to decode resp data")
+		c.JSON(http.StatusBadRequest, t7Error.DecodeFail.WithDetail(err.Error()))
+		return
+	}
+
+	if err := wallet.New().Withdraw(c, c.Param("walletId"), req.GetCurrency(), req.GetAmount()); err != nil {
+		defer c.Abort()
+		log.WithError(err).Error("fail to update user info")
+		t7Err, ok := t7Error.ToT7Error(err)
+		if !ok {
+			log.WithError(err).Error("unknown error")
+			c.JSON(http.StatusForbidden, t7Error.InvalidToken)
+			return
+		}
+		c.JSON(t7Err.GetStatus(), t7Err)
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
 }
--- internal/wallet/service.go
+++ internal/wallet/service.go
@@ -3,9 +3,11 @@ package wallet
 import (
 	"context"
 	"github.com/Template7/backend/internal/db"
+	"github.com/Template7/backend/internal/db/entity"
 	"github.com/Template7/backend/internal/t7Error"
 	"github.com/Template7/common/logger"
 	v1 "github.com/Template7/protobuf/gen/proto/template7/wallet"
+	"github.com/shopspring/decimal"
 	"sync"
 )
 
@@ -42,4 +44,38 @@ func (s *Service) GetWallet(ctx context.Context, walletId string) (v1.Wallet, er
 	}
 
 	return *wallet.ToProto(ctx), nil
+}
+
+func (s *Service) Deposit(ctx context.Context, walletId string, currency v1.Currency, amount uint32) error {
+	log := s.log.WithContext(ctx).With("walletId", walletId)
+	log.Debug("deposit")
+
+	m := entity.Money{
+		Currency: v1.Currency_name[int32(currency)],
+		Amount:   decimal.NewFromInt32(int32(amount)),
+	}
+	if err := s.db.Deposit(ctx, walletId, m); err != nil {
+		log.WithError(err).Error("fail to deposit")
+		return t7Error.DbOperationFail.WithDetail(err.Error())
+	}
+
+	log.Debug("deposit success")
+	return nil
+}
+
+func (s *Service) Withdraw(ctx context.Context, walletId string, currency v1.Currency, amount uint32) error {
+	log := s.log.WithContext(ctx).With("walletId", walletId)
+	log.Debug("withdraw")
+
+	m := entity.Money{
+		Currency: v1.Currency_name[int32(currency)],
+		Amount:   decimal.NewFromInt32(int32(amount)),
+	}
+	if err := s.db.Withdraw(ctx, walletId, m); err != nil {
+		log.WithError(err).Error("fail to withdraw")
+		return t7Error.DbOperationFail.WithDetail(err.Error())
+	}
+
+	log.Debug("withdraw success")
+	return nil
 }
