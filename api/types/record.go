b334811152231bf45f1490101adfdfdf857d65f1
allenkao <kaodondon7@gmail.com>
2023-12-05 18:30:02 +0800

    add api struct

---
+++ api/types/record.go
@@ -0,0 +1,20 @@
+package types
+
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
+
+type HttpGetWalletBalanceRecordResp struct {
+	HttpRespBase
+	Data []HttpGetWalletBalanceRecordRespData
+}
+
+type HttpGetWalletBalanceRecordRespData struct {
+	RecordId     int64           `json:"recordId"`
+	Io           string          `json:"io"` // in/out
+	AmountBefore decimal.Decimal `json:"amountBefore"`
+	AmountAfter  decimal.Decimal `json:"amountAfter"`
+	Timestamp    time.Time       `json:"timestamp"` // record created_at
+	Note         string          `json:"note"`
+}
